cf6ddcd2e6084320c1c43b6e48d98416d6b589b2
Alexander Tiekötter <alexander.tiekoetter@cern.ch>
2025-11-27 11:38:52 +0100

    [ALICE3, PWGDQ] Implementation of DQ data model and workflows for ALICE3 studies

--- PWGDQ/Core/CutsLibrary.cxx
+++ PWGDQ/Core/CutsLibrary.cxx
@@ -3703,6 +3703,46 @@ AnalysisCompositeCut* o2::aod::dqcuts::GetCompositeCut(const char* cutName)
     return cut;
   }
 
+  //---------------------------------------------------------------
+  // ALICE 3 studies composite cuts
+
+  if (!nameStr.compare("alice3StandardKine")) {
+    cut->AddCut(GetAnalysisCut("alice3StandardKine"));
+    return cut;
+  }  
+
+  if (!nameStr.compare("alice3KineSkim")) {
+    cut->AddCut(GetAnalysisCut("alice3KineSkim"));
+    return cut;
+  }  
+
+  if (!nameStr.compare("alice3TrackQuality")) {
+    cut->AddCut(GetAnalysisCut("alice3TrackQuality"));
+    return cut;
+  }  
+
+  if (!nameStr.compare("alice3iTOFPIDEle")) {
+    cut->AddCut(GetAnalysisCut("alice3iTOFPIDEle"));
+    return cut;
+  }  
+
+  if (!nameStr.compare("alice3oTOFPIDEle")) {
+    cut->AddCut(GetAnalysisCut("alice3oTOFPIDEle"));
+    return cut;
+  }  
+
+  if (!nameStr.compare("alice3RICHPIDEle")) {
+    cut->AddCut(GetAnalysisCut("alice3RICHPIDEle"));
+    return cut;
+  }  
+
+  if (!nameStr.compare("alice3DielectronPID")) {
+    cut->AddCut(GetAnalysisCut("alice3StandardKine"));
+    cut->AddCut(GetAnalysisCut("alice3TrackQuality"));
+    cut->AddCut(GetAnalysisCut("alice3RICHPIDEle"));
+    return cut;
+  }
+
   delete cut;
   LOGF(fatal, Form("Did not find cut %s. Returning nullptr", cutName));
   return nullptr;
@@ -6699,6 +6739,47 @@ AnalysisCut* o2::aod::dqcuts::GetAnalysisCut(const char* cutName)
     cut->AddCut(VarManager::kTPCnSigmaEl_Corr, -2.0, 4.0, false, VarManager::kPin, 3.0, 9999.0);
     cut->AddCut(VarManager::kTPCnSigmaPi_Corr, 2.5, 999, false, VarManager::kPin, 0, 2.0);
     cut->AddCut(VarManager::kTPCnSigmaPr_Corr, 2.5, 999, false, VarManager::kPin, 0, 2.0);
+    return cut;
+  }
+
+  //---------------------------------------------------------------
+  // ALICE 3 Analysis Cuts
+
+  if (!nameStr.compare("alice3KineSkim")) {
+    cut->AddCut(VarManager::kPt, 0.05, 1000.0);
+    cut->AddCut(VarManager::kEta, -4.0, 4.0);
+    return cut;
+  }
+
+  if (!nameStr.compare("alice3StandardKine")) {
+    cut->AddCut(VarManager::kPt, 0.1, 1000.0);
+    cut->AddCut(VarManager::kEta, -1.75, 1.75); // Value taken from LoI
+    return cut;
+  }
+  
+  if(!nameStr.compare("alice3TrackQuality")) {
+    cut->AddCut(VarManager::kTrackDCAxy, -1.0, 1.0);
+    cut->AddCut(VarManager::kTrackDCAz, -1.0, 1.0);
+    return cut;
+  }
+
+  if (!nameStr.compare("alice3iTOFPIDEle")) {
+    cut->AddCut(VarManager::kInnerTOFnSigmaEl, -3.0, 3.0);
+    cut->AddCut(VarManager::kInnerTOFnSigmaPi, -3.0, 3.0, true);
+    cut->AddCut(VarManager::kInnerTOFnSigmaPr, -3.0, 3.0, true);
+    return cut;
+  }
+
+  if (!nameStr.compare("alice3oTOFPIDEle")) {
+    cut->AddCut(VarManager::kOuterTOFnSigmaEl, -3.0, 3.0);
+    cut->AddCut(VarManager::kOuterTOFnSigmaPi, -3.0, 3.0, true);
+    cut->AddCut(VarManager::kOuterTOFnSigmaPr, -3.0, 3.0, true);
+    return cut;
+  }
+
+  if (!nameStr.compare("alice3RICHPIDEle")) {
+    cut->AddCut(VarManager::kRICHnSigmaEl, -3.0, 3.0);
+    cut->AddCut(VarManager::kHasRICHSigEl, 0.5, 1.5);
     return cut;
   }
 
