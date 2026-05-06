24ff49544de9f0e403e3e50fb43b8504ca10c976
Alexander Tiekötter <alexander.tiekoetter@uni-muenster.de>
2026-05-06 18:19:36 +0200

    Refactoring of dqEfficiency_withAssoc_direct

--- PWGDQ/Tasks/dqEfficiency_withAssoc_direct.cxx
+++ PWGDQ/Tasks/dqEfficiency_withAssoc_direct.cxx
@@ -2019,7 +2019,7 @@ struct AnalysisSameEventPairing {
   PresliceUnsorted<aod::McParticles> perReducedMcEvent = aod::mcparticle::mcCollisionId;
 
   // template <int TPairType, typename TEventsMC>
-  template <int TPairType, typename TEvents, typename TEventsMC>
+  template <int TPairType, uint32_t TEvents, typename TEventsMC>
   void runMCGen(TEvents const& events, TEventsMC const& mcEvents, McParticles const& mcTracks)
   {
     cout << "AnalysisSameEventPairing::runMCGen() called" << endl;
@@ -2055,7 +2055,11 @@ struct AnalysisSameEventPairing {
       eFromJpsiMcParticleIndices.clear();
 
       auto mcCollisionGlobalIndex = event.mcCollisionId();
-      // auto mcEvent = mcEvents.rawIteratorAt(mcCollisionGlobalIndex);
+      auto mcEvent = mcEvents.rawIteratorAt(mcCollisionGlobalIndex);
+
+      // fill event information
+      VarManager::FillEvent<TEventFillMap>(event);
+      VarManager::FillEvent<VarManager::ObjTypes::CollisionMC>(mcEvent);
 
       auto groupedMCTracks = mcTracks.sliceBy(perReducedMcEvent, mcCollisionGlobalIndex);
       groupedMCTracks.bindInternalIndicesTo(&mcTracks);
@@ -2161,20 +2165,16 @@ struct AnalysisSameEventPairing {
                          soa::Join<aod::TrackAssoc, aod::BarrelTrackCuts, aod::Prefilter> const& barrelAssocs,
                          MyBarrelTracksWithCovWithAmbiguities const& barrelTracks, McCollisions const& mcEvents, McParticles const& mcTracks)
   {
-    cout << "AnalysisSameEventPairing::processBarrelOnly() called" << endl;
     runSameEventPairing<true, VarManager::kDecayToEE, gkEventFillMapWithMults, gkTrackFillMapWithCov>(events, bcs, trackAssocsPerCollision, barrelAssocs, barrelTracks, mcEvents, mcTracks);
-    runMCGen<VarManager::kDecayToEE>(events, mcEvents, mcTracks);
-    cout << "AnalysisSameEventPairing::processBarrelOnly() completed" << endl;
+    runMCGen<VarManager::kDecayToEE, gkEventFillMapWithMults>(events, mcEvents, mcTracks);
   }
 
   void processBarrelPbPbOnly(MyEventsSelectedWithCentAndMults const& events, BCsWithTimestamps const& bcs,
                              soa::Join<aod::TrackAssoc, aod::BarrelTrackCuts, aod::Prefilter> const& barrelAssocs,
                              MyBarrelTracksWithCovWithAmbiguities const& barrelTracks, soa::Join<aod::McCollisions, aod::McCollsExtra, aod::MultMCExtras> const& mcEvents, McParticles const& mcTracks)
   {
-    cout << "AnalysisSameEventPairing::processBarrelPbPbOnly() called" << endl;
     runSameEventPairing<true, VarManager::kDecayToEE, gkEventFillMapWithCentAndMults, gkTrackFillMapWithCov>(events, bcs, trackAssocsPerCollision, barrelAssocs, barrelTracks, mcEvents, mcTracks);
-    runMCGen<VarManager::kDecayToEE>(events, mcEvents, mcTracks);
-    cout << "AnalysisSameEventPairing::processBarrelPbPbOnly() completed" << endl;
+    runMCGen<VarManager::kDecayToEE, gkEventFillMapWithCentAndMults>(events, mcEvents, mcTracks);
   }
 
   void processDummy(MyEvents&)
